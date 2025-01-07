6895d9fe8965b3e65575f4753434893eb0ee46e6
soy-anu <wijesundarachamoda@gmail.com>
2025-01-07 16:31:48 +0530

    changes to stg

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 number = range(int(input("please enter a number here \n")))
-
+name = input("enter your name")
 
 for i in number :
-    print("hello there")
+    print("hello there" + ' ' + name)
     
 
